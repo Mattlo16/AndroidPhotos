b90cb2373865c005b27cf2fb2c4e88ce508972fa
Sean Dong <seandong534@gmail.com>
2025-05-02 19:29:45 -0400

    Update MainActivity.java

--- app/src/main/java/com/cs213/androidphotos/MainActivity.java
+++ app/src/main/java/com/cs213/androidphotos/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        AppDataManager.getInstance().initialize(this);
+        AppDataManager.getInstance(this).initialize(this);
         navigateToAlbumList();
     }
 
